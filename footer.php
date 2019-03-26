2c1eeaa0861101a350546a0f1ba7935f3cc09453
bologia <g.gusberti@simplon-charleville.fr>
2019-03-26 15:24:32 +0100

    yo

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer>
+  <link href="styles/footer1.css" rel="stylesheet" type="text/css">
+
+  <div class="footer">
+    <div class="foot">
+      <div class="logofoot">
+    <a href="index.php">
+      <img src="images/logo_(copie1).png" width=90px /> </a>
+    </div>
+
+    <div class="reseauxsocio">
+    <a href="https://www.facebook.com/Simplon.co/?fref=nf">
+      <img src="images/facebookr.png" width=110px /> </a>
+    <a href="https://twitter.com/simplonco">
+      <img src="images/twitterr.png" width=110px /> </a>
+    <a href="https://www.youtube.com/channel/UCTFYd2sOfjrJH1C421UyYuQ">
+      <img src="images/youtuber.png" width=110px /> </a>
+      </div>
+    </div>
+  </div>
+
+
+</footer>
