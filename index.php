266a86ac2dfe2a7fb8585615e524fd2c7779880c
PC-Simplon <c.servieres@simplon-charleville.fr>
2019-03-26 14:05:56 +0100

    lier le header et le footer

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+  <title>Accueil</title>
+  <meta charset="utf8"/>
+  <link href="styles/index.css" type="text/css"
+  rel="stylesheet">
+</head>
+<html>
+<body>
+  <div class="header">
+  <?php include 'header.php'; ?>
+  </div>
+  <div class="footer">
+  <?php include 'footer.php'; ?>
+  </div>
+</body>
+</html>
