0f3dd5424c5d583873968aea032f4b27a74114f6
PC-Simplon <c.servieres@simplon-charleville.fr>
2019-03-26 13:54:17 +0100

    nouveaux fichiers

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+<link href="styles/header.css" type="text/css"
+  rel="stylesheet">
+  <div class="menu">
+    <div class= "logo"><img src="images/logo.png"></div>
+    <a href="index.php"><div class="accueil">Accueil</div></a>
+    <a href="resto.php"><div id="div3">Nos Restaurants</div></a>
+    <a href="contact.php" target="_blank"><div class="contact">Contact</div></a>
+    </div>
+  </div>
+</header>
