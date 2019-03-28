2f12a8f39b0e9839c36728d749c0b641e96df061
PC-Simplon <c.servieres@simplon-charleville.fr>
2019-03-28 14:20:46 +0100

    ajout de la page d'accueil et corrections

--- footer.php
+++ footer.php
@@ -6,16 +6,16 @@
     <div class="foot">
       <div class="logofoot">
     <a href="index.php">
-      <img src="images/logo_(copie1).png" width=90px /> </a>
+      <img src="images/logofooter.png" width=90px /> </a>
     </div>
 
     <div class="reseauxsocio">
     <a href="https://www.facebook.com/Simplon.co/?fref=nf">
-      <img src="images/facebookr.png" width=110px /> </a>
+      <img src="images/facebook.png" width=110px /> </a>
     <a href="https://twitter.com/simplonco">
-      <img src="images/twitterr.png" width=110px /> </a>
+      <img src="images/twitter.png" width=110px /> </a>
     <a href="https://www.youtube.com/channel/UCTFYd2sOfjrJH1C421UyYuQ">
-      <img src="images/youtuber.png" width=110px /> </a>
+      <img src="images/youtube.png" width=110px /> </a>
       </div>
     </div>
   </div>
